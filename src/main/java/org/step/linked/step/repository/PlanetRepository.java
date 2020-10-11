ffa850bfeb20b64f64114ad49ceefc07cf806e60
Gleb <asd>
2020-10-11 10:19:09 +0600

    asd

---
+++ src/main/java/org/step/linked/step/repository/PlanetRepository.java
@@ -0,0 +1,8 @@
+package org.step.linked.step.repository;
+
+import org.springframework.stereotype.Repository;
+import org.step.linked.step.entity.Planet;
+
+@Repository
+public interface PlanetRepository extends LinkedCommonRepository<Planet>{
+}
